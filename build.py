f29f2cf40d65aa032e412f751b70e4d188dcc465
Narendran Kumaragurunathan <narendran.kumaragurunathan@amd.com>
2019-07-12 12:42:30 -0400

    build fixes

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ import os
 debussy_home = os.environ["VERDI_HOME"];
 
 inc_dir = [debussy_home + '/share/FsdbReader', os.environ["ANCHOR_surfboard"] + os.sep + 'lib/creg', os.environ["ANCHOR_surfboard"] + os.sep + 'lib/pyc'];
-libs = [debussy_home + '/share/FsdbReader/LINUXAMD64/libnffr.a', debussy_home + '/share/FsdbReader/LINUXAMD64/libnsys.a'];
+libs = [debussy_home + '/share/FsdbReader/LINUXAMD64/libnffr.so', debussy_home + '/share/FsdbReader/LINUXAMD64/libnsys.so', '-lz'];
 
 setup (name="sb", version="1.0",
       ext_modules=[Extension("sb", [os.environ["ANCHOR_surfboard"] + os.sep + "lib/pyc/sb.cc"], extra_compile_args=[], define_macros=[("NO_ENV_DKI", None), ("__CREG_MSG__", None)], include_dirs=inc_dir, extra_objects=libs)]);
